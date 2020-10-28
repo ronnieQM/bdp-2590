f895e83833ab9c553e52f4dc2bb90db62f7b04a7
Ronnie <roniquinonez@icloud.com>
2020-10-28 09:31:00 -0700

    .

--- scripts/reading.py
+++ scripts/reading.py
@@ -5,13 +5,23 @@ import csv
 import xml.etree.ElementTree as ET
 
 # scripts directory, inside of project directory
-curr_path =os.getcwd()
+curr_path = os.getcwd()
 # home this project files
 project_path = os.path.dirname(curr_path)
 # project_path/data
 data_path = os.path.join(project_path, 'data')
 
-path,dirs,files=next(os.walk(data_path))
+path, dirs, files = next(os.walk(data_path))
 
 print(len(files))
 print('there are {} xml files in the data dir')
+
+def read_xmls():
+    print('nothing')
+
+def read_xsds():
+    print('nothing')
+
+
+if __name__ == '__main__':
+    main()
