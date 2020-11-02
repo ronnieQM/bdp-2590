75c23c954d74ba3205a5126936d6d74a7da2b9af
Ronnie <roniquinonez@icloud.com>
2020-11-01 18:31:01 -0800

    .

--- scripts/data_dictionary_parser.py
+++ scripts/data_dictionary_parser.py
@@ -23,24 +23,11 @@ status_dir = os.path.join(data_dir, 'status')
 xsds_dir = os.path.join(project_dir, 'xsds')
 status_xsd = os.path.join(xsds_dir, 'StandardStatusExport.xsd')
 
-################################
-######## metadata
-################################
 path, dirs, files = next(os.walk(status_dir))
-print('#' * 40)
-print("""
-        there are {} xml files in the data/status dir
 
-        this project directory: {} 
-        this python script's directory: {}"""
-      .format(len(files), project_dir, os.getcwd()))
-print('#' * 40)
-
-standard_carrier_data_dic = '/home/rqm/main/dev/ace/bdp-2590/xact_minus/XactAnalysis Export Documentation/Estimate Export/StandardCarrier data dictionary.xlsx'
+standard_carrier_data_dic = '/home/rqm/main/dev/ace/bdp-2590/xact-edi/XactAnalysis Export Documentation/Estimate Export/StandardCarrier data dictionary.xlsx'
 
 
-def read_xmls():
-    pass
 
 
 def read_xsds(xsd_file):
@@ -72,14 +59,12 @@ def read_xsds(xsd_file):
 
 def something(xml_file):
     x = xml_file
-
     status_dir = os.path.join(data_dir, 'status')
+    # all example XML files in 'file'
     path, dirs, file = next(os.walk(status_dir))
-    print(file[0])
 
     # get every possible data type from the documentation
     # get every row in the csv
-
     list_of_data_dict = []
     with open('test.csv')as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -87,29 +72,35 @@ def something(xml_file):
         pair = {}
         for row in csv_reader:
             if line_count == 0:
-                print('Column names are {}.'.format(', '.join(row)))
+                # print('Column names are {}.'.format('| '.join(row)))
                 line_count += 1
             else:
-                # print(row[0])
-                # print(row[1])
                 pair[row[0]] = row[1]
                 list_of_data_dict.append(pair)
 
-        print('row count: {}'.format(line_count))
+        # for x in list_of_data_dict:
+        #     for i, j in x.items():
+        #         print(i, ' : ', j)
 
-        for x in list_of_data_dict:
-            for i, j in x.items():
-                print(i, ' : ', j)
+
 
 
 def main():
     # read file
     read_file = pd.read_excel(standard_carrier_data_dic)
     read_file.to_csv('test.csv', index=None, header=True)
-    print('shit!')
 
-    something('asdf ')
+    something('nothing')
 
+    print('#' * 40)
+    print("""metadata
+            there are {} xml files in the data/status dir
+
+            this project directory: {} 
+            this python script's directory: {}
+    /metadata"""
+          .format(len(files), project_dir, os.getcwd()))
+    print('#' * 40)
 
 if __name__ == '__main__':
     main()
