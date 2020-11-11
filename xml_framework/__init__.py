947181942cc435effae5fcd93a206a8410e563c2
Ronnie <roniquinonez@icloud.com>
2020-11-11 15:15:23 -0800

    .

---
+++ xml_framework/__init__.py
@@ -0,0 +1,6 @@
+# from .xml_framework import xml2db
+# from .xml_framework import sql_formatter
+
+from .xml_framework.sql_formatter import field_formatter,generate_insert_query, generate_create_query, values_formatter
+from .xml_framework.xml2db import get_tid, data2db, get_elem_count, recursive_search, recursive_iterate, \
+    get_parent_of_type, generate_bd_schema
---
+++ xml_framework/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xml_framework",
+    version="0.0.1",
+    author="Roni Quinonez",
+    author_email="",
+    description="package used to move xml data into RDBS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7.5",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7.5',
+)
