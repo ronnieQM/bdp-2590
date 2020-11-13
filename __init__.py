1298714711a49e5cbfe411e4c6ed73b9f701dfe0
Ronnie <roniquinonez@icloud.com>
2020-11-13 14:36:20 -0800

    .

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# from acexml.xml_parser import *
+# from acexml import sql_formatter
+
+# import acexml.xml_parser as xml_parser
+# import acexml
+# from .acexml import xml_parser
+# from .acexml.xml_parser import generate_db_schema
+# from .acexml
+
+# from acexml.xml_parser import generate_db_schema
+# from .acexml.xml_parser import generate_db_schema
+
+# from .acexml import xml_parser
+#
+from .acexml import xml_parser
+from .acexml.xml_parser import generate_db_schema, get_etree
--- dev/solutions/main_xlm.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-from .solutions.xlm2rdbs import *
-from .solutions.sql_formatter import field_formatter, generate_create_query, values_formatter,generate_insert_query
-from .solutions.quick_tools import breakout,clearit,breakpoint
-
-from prettytable import PrettyTable
